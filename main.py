4da451fceaf71c0d2cb2b71dbcc7c730f4650ad9
Robin <rgj.duprat@gmail.com>
2019-12-19 18:46:01 +0100

    Affiche l'inspector

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 import sys
-import draw, file_airport
+import draw, airport_inspector
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
 
     scene = draw.DrawAirport()
+    inspector = airport_inspector.AirportInspector()
+    inspector_dock = QtWidgets.QDockWidget()
+    inspector_dock.setWidget(inspector)
 
     window = QtWidgets.QMainWindow()
     window.setWindowTitle("Airport creation")
     window.setCentralWidget(scene)
+    window.addDockWidget(QtCore.Qt.DockWidgetArea(1), inspector_dock)
     window.showMaximized()
 
     sys.exit(app.exec_())
