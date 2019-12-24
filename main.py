71e34df58116c7d1467bcbcb2758334989705662
Robin <rgj.duprat@gmail.com>
2019-12-24 15:44:40 +0100

    Revert "Revert "Merge remote-tracking branch 'origin/master'""
    This reverts commit 26ba107124d437034b03624b4800373fad513b93.

--- main.py
+++ main.py
@@ -1,20 +1,56 @@
+"""Main module for the Python Airport code"""
+
 from PyQt5 import QtWidgets, QtCore
-import sys
-import draw, airport_inspector
 
+import airport
+import inspector
+import radarview
+import simulation
+import traffic
 
-if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
+APT_FILE = ("DATA/lfpg_map.txt", "DATA/lfpo_map.txt")
+PLN_FILE = ("DATA/lfpg_flights.txt", "DATA/lfpo_flights.txt")
 
-    scene = draw.DrawAirport()
-    inspector = airport_inspector.AirportInspector()
-    inspector_dock = QtWidgets.QDockWidget()
-    inspector_dock.setWidget(inspector)
+if __name__ == "__main__":
+    # Load files
+    # choice = 0 if input("1: Roissy / [2: Orly] ? ") == '1' else 1
+    choice = 0
+    apt = airport.from_file(APT_FILE[choice])
+    flights = traffic.from_file(apt, PLN_FILE[choice])
 
-    window = QtWidgets.QMainWindow()
-    window.setWindowTitle("Airport creation")
-    window.setCentralWidget(scene)
-    window.addDockWidget(QtCore.Qt.DockWidgetArea(1), inspector_dock)
-    window.showMaximized()
+    # create the simulation
+    sim = simulation.Simulation(apt, flights)
 
-    sys.exit(app.exec_())
+    # Initialize Qt
+    app = QtWidgets.QApplication([])
+
+    # create the radar view and the time navigation interface
+    rad = radarview.RadarView(sim)
+    rad.move(10, 10)
+
+    # create the inspector
+    insp = inspector.Inspector(rad)
+
+    # create a QDockWidget for the inspector
+    insp_dock = QtWidgets.QDockWidget()
+    insp_dock.setWidget(insp)
+
+    # create the QMainWindow and add both widgets
+    win = QtWidgets.QMainWindow()
+    win.setWindowTitle("AirPort Sim Qt MainWindow & Dock")
+    win.setCentralWidget(rad)
+    win.addDockWidget(QtCore.Qt.DockWidgetArea(1), insp_dock)
+    # win.resize(1000, 600)
+    # win.show()
+    win.showMaximized()
+
+    # create the second view
+    # second_view = radarview.PanZoomView(main_window.scene)
+    # second_view.scale(0.1, -0.1)
+    # second_view.show()
+    # second_view.move(300, 300)
+
+    print(simulation.SHORTCUTS)
+
+    # enter the main loop
+    app.exec_()
